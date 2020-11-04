bebc2c03797e01426998056d43fe6b73f1615586
thatCbin <1172816325@qq.com>
2020-11-04 14:17:03 +0800

    Initial commit

---
+++ app/src/main/java/com/donkingliang/consecutivescrollerdemo/MainActivity.java
@@ -0,0 +1,73 @@
+package com.donkingliang.consecutivescrollerdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_sample).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SampleActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.btn_sticky).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,StickyActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.btn_sink_sticky).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SinkStickyActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.btn_consecutive).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ConsecutiveActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.btn_viewpager).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ViewPagerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.btn_sticky_permanent).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PermanentStickyActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.btn_fragment).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FragmentActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/donkingliang/consecutivescrollerdemo/MyFragment.java
@@ -0,0 +1,48 @@
+package com.donkingliang.consecutivescrollerdemo;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.donkingliang.consecutivescrollerdemo.adapter.RecyclerViewAdapter;
+import com.scwang.smart.refresh.layout.SmartRefreshLayout;
+
+/**
+ * @Author donkingliang
+ * @Description
+ * @Date 2020/4/18
+ */
+public class MyFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_my,container,false);
+
+        RecyclerView recyclerView1 = view.findViewById(R.id.recyclerView1);
+        recyclerView1.setLayoutManager(new LinearLayoutManager(getActivity()));
+        RecyclerViewAdapter adapter1 = new RecyclerViewAdapter(getActivity(),"RecyclerView1-");
+        recyclerView1.setAdapter(adapter1);
+
+        RecyclerView recyclerView2 = view.findViewById(R.id.recyclerView2);
+        recyclerView2.setLayoutManager(new LinearLayoutManager(getActivity()));
+        RecyclerViewAdapter adapter2 = new RecyclerViewAdapter(getActivity(),"RecyclerView2-");
+        recyclerView2.setAdapter(adapter2);
+        return view;
+
+    }
+
+    public void onLoadMore(final SmartRefreshLayout layout){
+        // 模拟加载5秒钟
+        layout.finishLoadMore(5000);
+    }
+}
